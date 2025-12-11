d737cffa284d1cc53dcb1720870d1d39d47c8240
mdkrazymdk <49284825+mdkrazymdk@users.noreply.github.com>
2025-12-11 19:37:31 +0100

    Added Score class logic
    Removed comments in Ukrainian from Score class.

--- pr3/Score.py
+++ pr3/Score.py
@@ -3,17 +3,17 @@ from tkinter import *
 class Score:
     def __init__(self, canvas):
         self.canvas = canvas
-        self.score = 0  # Спіймані
-        self.lost = 0   # Пропущені
+        self.score = 0  
+        self.lost = 0   
         self.text = None
         self.show_text()
 
     def show_text(self):
-        # Якщо тексту ще немає, створюємо його
+        
         if self.text is None:
             self.text = self.canvas.create_text(350, 20, text=f"Спіймав: 0 Пропустив: 0", font=('Helvetica', 16), fill='black')
         else:
-            # Якщо є, оновлюємо
+       
             self.canvas.itemconfig(self.text, text=f"Спіймав: {self.score} Пропустив: {self.lost}")
 
     def catched_egg(self):
