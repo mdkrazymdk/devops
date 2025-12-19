5d6063eded9146b903ed3b19c0aa38738d6a525b
mdkrazymdk <49284825+mdkrazymdk@users.noreply.github.com>
2025-12-19 13:38:45 +0100

    Refactor views to add tagging and similar posts
    Refactored post list view to support tagging and pagination. Added similar posts feature in post detail view.

--- pr5/blog/views.py
+++ pr5/blog/views.py
@@ -5,12 +5,29 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag': tag})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
@@ -21,19 +38,23 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
 
     comments = post.comments.filter(active=True)
- 
     form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+        .order_by('-same_tags', '-publish')[:4]
 
     return render(request, 'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
-
-# Функція для відправки поста на email [cite: 6376-6391]
+                   'form': form,
+                   'similar_posts': similar_posts})
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     sent = False
-    
+
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
@@ -42,12 +63,11 @@ def post_share(request, post_id):
             subject = f"{cd['name']} recommends you read {post.title}"
             message = f"Read {post.title} at {post_url}\n\n" \
                       f"{cd['name']}'s comments: {cd['comments']}"
-         
             send_mail(subject, message, 'your_account@gmail.com', [cd['to']])
             sent = True
     else:
         form = EmailPostForm()
-        
+
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
 @require_POST
@@ -55,12 +75,12 @@ def post_comment(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     comment = None
     form = CommentForm(data=request.POST)
-    
+
     if form.is_valid():
         comment = form.save(commit=False)
         comment.post = post
         comment.save()
-        
+
     return render(request, 'blog/post/comment.html',
                   {'post': post,
                    'form': form,
